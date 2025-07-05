634dad0c9aa7d38f291669289dfd596cdde1f7c0
aung_myat <gusion762389@gmail.com>
2025-07-05 20:44:42 +0630

    fix cors again

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/uploads/img", express.static(path.join(__dirname, "uploads", "img")));
 app.use(
     cors({
-        origin: "https://destinate-mern-backend-production.up.railway.app", // your React app domain
+        origin: process.env.origin || "http://localhost:3000", // your React app domain
         credentials: true, // allow cookies if needed
     })
 );
